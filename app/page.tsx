00d4b32c160a8be4d9e51ef8c737678a44b91d03
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-11-15 17:42:33 +0700

    3.8. Sử dụng Image trong Next.js (với next/image)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl text-center">Hello</h1>
+      <div className="w-[700px] h-[700px] bg-red-300">
+        <Image
+          src="https://i.pinimg.com/736x/56/1e/8e/561e8eac54e582cfae853bdb8b586663.jpg"
+          alt="yi.jpg"
+          width={500}
+          height={500}
+        />
+      </div>
     </main>
   )
 }
