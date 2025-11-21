4cbf82e7baa92ba4beb724832281141ef435d714
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-11-21 10:23:07 +0700

    3.27. Render trang danh sách sản phẩm bằng Server Component | Khóa học Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowLocalIP: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+      },
+    ],
+  },
+}
 
 export default nextConfig
